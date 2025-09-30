38166d3fe1f3c7a4493806cd8494a0f7b4763878
Erick Diaz <ediazluc@gmail.com>
2025-09-30 18:34:17 -0500

    Add files via upload

--- comp324ClassExercises/week5/exercise15/assets/app.js
+++ comp324ClassExercises/week5/exercise15/assets/app.js
@@ -16,7 +16,22 @@ function main() {
     text.appendChild(outputText);
     linearFuncOutput.appendChild(text);
   });
-}
+
+
+  //Exercise part 4 Html and JS
+  // add event listener for keypress in note input field
+    const textTyper = document.getElementById("textTyping");
+      //[65, 90] Uppercase, [97,122] lowercase (ASCII VALUES!)
+      textTyper.addEventListener("keypress", (e) => {
+        if ((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 97 && e.keyCode <= 122)) {
+            console.log("LETTERS AND MORE LETTERS!!!..."); 
+        }
+
+        if(e.keyCode === 13){ //enter key!!!
+          console.log("ENTER KEY WAS PRESSED!!!");
+        }
+    });
+  }
 
 // start main()
 main();
